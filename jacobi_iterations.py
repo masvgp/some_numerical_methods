6adf4b2a7738d579c84e61c31952b22dabe49071
masvgp <masvgp@users.noreply.github.com>
2020-09-05 20:27:16 -0600

    added jacobiteration method for solving systems of equations.

---
+++ jacobi_iterations.py
@@ -0,0 +1,50 @@
+# Jacobi iterations method for solving Ax=b
+import numpy as np
+
+
+def jacobi(A, b, x_start, epsilon=1e-10, max_iterations=500):
+  """
+  Jacobi iteration method for solving the system of equations Ax=b.
+  The Matrix equation is x_next = D^{-1} * (b - M * x_previous) where D^{-1} is the inverse of the diagonal elements of A, M is the remaining elements of A after the diagonals are subtracted, and b is b.
+  A : matrix
+      Numpy array containing the numbers in the coefficient matrix.
+  b : vector
+      Vector containing the numbers on the right-hand side of Ax=b.
+  returns : vector
+            Function returns a solution vector x.
+  """
+  # select out diagonal elements of A
+  D = np.eye(A.shape[0], A.shape[1])
+  D = D * A.diagonal()
+  # Find D^{-1}
+  D_inv = np.diag(1 / np.diag(D))
+
+  M = A - D
+  x = x_start
+  for i in range(max_iterations):
+    x_next = np.dot(D_inv, b - np.dot(M, x))
+    print(x_next)
+
+    if np.linalg.norm(x - x_next) < epsilon:
+      return x
+    x = x_next
+
+  return x
+
+# Test the function using data from Wikipedia
+# problem data
+A = np.array([
+    [5, 2, 1, 1],
+    [2, 6, 2, 1],
+    [1, 2, 7, 1],
+    [1, 1, 2, 8]
+])
+b = np.array([29, 31, 26, 19])
+
+# you can choose any starting vector
+x_start = np.zeros(len(b))
+x = jacobi(A, b, x_start)
+
+print("x:", x)
+print("computed b:", np.dot(A, x))
+print("real b:", b)
--- newton_raphson_func.py
+++ newton_raphson_func.py
@@ -2,55 +2,59 @@
 import numpy as np
 from finite_differences import finite_difference
 
+# Create empty list to store x_k.
+guessed = []
+
 
 def newton_raphson(f, start, h, tol, dydx='center_diff'):
-    """
-    Implementation of the Newton-Raphson algorithm for zero finding.
-    f : function
-        Function for which the root is to be found.
-    x : number or vector
-        Either a number for which the finite difference is to be computed, or a vector of values over which to compute the differences of f.
-    h : number
-        Step size in difference formula
-    dydx : a derivative or finite difference method.
-           Choices include 'center_diff', 'back_diff', and 'forward_diff'.
-           Defaults to the 'center_diff' method.
-           The user may also define their derivative explicitly as a function of x.
-    tol : number
-          Threshold past which the algorithm terminates.
-    start : number
-            Starting guess.
-    """
-    # Set derivative method.
-    if dydx == 'center_diff':
-        dydx = finite_difference(f, start, h).center_diff()
-    elif dydx == 'back_diff':
-        dydx = finite_difference(f, start, h).back_diff()
-    elif dydx == 'forward_diff':
-        dydx = finite_difference(f, start, h).forward_diff()
-    else:
-        dydx = dydx(start)
+  """
+  Implementation of the Newton-Raphson algorithm for zero finding.
+  f : function
+      Function for which the root is to be found.
+  x : number or vector
+      Either a number for which the finite difference is to be computed, or a vector of values over which to compute the differences of f.
+  h : number
+      Step size in difference formula
+  dydx : a derivative or finite difference method.
+         Choices include 'center_diff', 'back_diff', and 'forward_diff'.
+         Defaults to the 'center_diff' method.
+         The user may also define their derivative explicitly as a function of x.
+  tol : number
+        Threshold past which the algorithm terminates.
+  start : number
+          Starting guess.
+  """
 
-    # Check to see if initial guess is already within tolerance.
-    if abs(f(start)) < tol:
-        return str(start) + " is a zero of f."
-    # Compute next x value and recursively call newton_raphson.
-    else:
-        x_next = start - f(start) / dydx
-        print(x_next)
-        return newton_raphson(f=f,
-                              start=x_next,
-                              h=h,
-                              tol=tol,
-                              dydx='center_diff')
+  # Set derivative method.
+  if dydx == 'center_diff':
+    dydx = finite_difference(f, start, h).center_diff()
+  elif dydx == 'back_diff':
+    dydx = finite_difference(f, start, h).back_diff()
+  elif dydx == 'forward_diff':
+    dydx = finite_difference(f, start, h).forward_diff()
+  else:
+    dydx = dydx(start)
+
+  # Check to see if initial guess is already within tolerance.
+  if abs(f(start)) < tol or (start in guessed):
+    return str(start) + " is a zero of f."
+  # Compute next x value and recursively call newton_raphson.
+  else:
+    guessed.append(start)
+    x_next = start - f(start) / dydx
+    return newton_raphson(f=f,
+                          start=x_next,
+                          h=h,
+                          tol=tol,
+                          dydx='center_diff')
 
 
 # Test code
-def f(x):
-    return x**2
+# def f(x):
+#   return x**2
 
 
-start = -4
-h = 0.01
-tol = 0.0001
-print(newton_raphson(f, start, h, tol, dydx=lambda x: 2 * x))
+# start = -4
+# h = 0.01
+# tol = 0.0001
+# print(newton_raphson(f, start, h, tol, dydx=lambda x: 2 * x))
